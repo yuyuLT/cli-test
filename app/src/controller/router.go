d42b99c5da23cfe745ea4b2154807fa3cddf4fa6
yuyuLT <wawawawadaiko@icloud.com>
2022-10-13 16:46:14 +0900

    change directory and add css

--- app/src/controller/router.go
+++ app/src/controller/router.go
@@ -7,7 +7,7 @@ import (
 func GetRouter() *gin.Engine {
 	r := gin.Default()
 	r.LoadHTMLGlob("view/*html")
-	r.Static("images", "./images")
+	r.Static("assets", "./assets")
 	r.GET("/", ShowMessage)
 	return r
 }
